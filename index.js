27631d5c03be28d3c0ecfbfc987aaf542c64e5f9
Salah Uddin <77015008+SalahUddin7@users.noreply.github.com>
2021-11-29 19:59:02 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Color controls
+
+var purpleBtn = document.getElementById("purple")
+var greenBtn = document.getElementById("green")
+var blueBtn = document.getElementById("blue")
+var redBtn = document.getElementById("red")
+
+purple.addEventListener("click", function() {
+    document.body.style.backgroundColor = "rebeccapurple"
+})
+
+green.addEventListener("click", function() {
+    document.body.style.backgroundColor = "forestgreen"
+})
+
+blue.addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue"
+})
+
+red.addEventListener("click", function() {
+    document.body.style.backgroundColor = "firebrick"
+})
+
+white.addEventListener("click", function() {
+    document.body.style.backgroundColor = "white"
+})
+
